78dfb2ec62f657158450f9f5ec35add9da8435bf
Johan Bendz <34488771+JohanBendz@users.noreply.github.com>
2018-12-14 21:49:35 +0100

    Update device.js

--- drivers/FG-FR404-ZW-HF/device.js
+++ drivers/FG-FR404-ZW-HF/device.js
@@ -7,16 +7,21 @@ class HoppeSecuDoorWindowSensor extends ZwaveDevice {
 	async onMeshInit() {
 		
 		// enable debugging
-		// this.enableDebug();
+		this.enableDebug();
 		
 		// print the node's info to the console
-		// this.printNode();
+		this.printNode();
 
 		// register device capabilities
 		this.registerCapability('alarm_contact', 'NOTIFICATION');
 		this.registerCapability('alarm_tamper', 'NOTIFICATION');
 		this.registerCapability('alarm_battery', 'BATTERY');
 		this.registerCapability('measure_battery', 'BATTERY');
+		
+		// register a report listener
+		this.registerReportListener('SWITCH_BINARY', 'SWITCH_BINARY_REPORT', ( rawReport, parsedReport ) => {
+		     console.log('registerReportListener', rawReport, parsedReport);
+		});
 	}
 }
 module.exports = HoppeSecuDoorWindowSensor;
