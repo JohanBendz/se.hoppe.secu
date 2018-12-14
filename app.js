0766e3cf84842b0bd18e9daa4a9263ce869ffe0b
Johan Bendz <34488771+JohanBendz@users.noreply.github.com>
2018-12-14 23:11:00 +0100

    Update app.js

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const Homey = require('homey');
 class hoppeSecuApp extends Homey.App {
 	
 	onInit() {
-		this.log('Hoppe Secu app is running...');
+		this.log('Hoppe SecuSignal app is running...');
 	}
 	
 }
